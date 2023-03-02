af9aada70d339a59a9ae61114d3638ec18ca6a1b
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-03-02 10:19:23 +0200

    update

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Box } from "@chakra-ui/react"
+export default function Privacy() {
+  return (
+    <Box>
+    Privacy Policy for Google Play - Music NFT Streaming App
+We take your privacy seriously and are committed to protecting your personal information. This privacy policy explains how we collect, use, and disclose your personal information when you use our Music NFT Streaming App available on Google Play.
+
+Information Collection and Use
+We do not collect any personal information from you when you use our Music NFT Streaming App. We do not require you to create an account, provide any personal information, or connect to any social media platforms to use our app.
+
+Third-Party Services
+We do not share your personal information with any third-party services or entities. We do not use any third-party analytics tools or advertising networks that collect your personal information.
+
+Security
+We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure. However, please be aware that no method of transmitting information over the internet or storing information is completely secure. We cannot guarantee the absolute security of your personal information.
+
+Children's Privacy
+Our Music NFT Streaming App is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that we have collected personal information from a child under 13, we will take steps to delete that information as soon as possible.
+
+Changes to this Privacy Policy
+We may update this privacy policy from time to time. If we make material changes to this policy, we will notify you by posting a notice in our app or by other means.
+
+Contact Us
+If you have any questions or concerns about our privacy policy, please contact us at [insert contact information].
+    </Box>
+  )
+}
