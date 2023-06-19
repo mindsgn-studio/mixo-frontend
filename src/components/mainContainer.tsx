00b8eead7ee98253cda93a6ec736fffb9e809aa0
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-06-19 11:32:44 +0200

    update

--- src/components/mainContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { Box, Flex, Center } from "@chakra-ui/react";
-import { SideNavigation } from "@/components/sideNavigation";
-import { HomeContainer } from "@/components/homeContainer";
-
-export const MainContainer = () => {
-  return <HomeContainer />;
-};
