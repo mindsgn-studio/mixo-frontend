6a2b17b358af00f78af664bfb2556d17bc60df9c
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-06-30 13:15:01 +0200

    infinite scroll

--- src/components/allTracksContainer.tsx
+++ src/components/allTracksContainer.tsx
@@ -1,36 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SimpleGrid, Container, Box } from "@chakra-ui/react";
 import { TrackCard } from "./trackCard";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { json } from "stream/consumers";
+import { AllTracksLoading } from "@/components/allTracksLoading";
 
 interface AllTracksContainerProps {
   tracks?: any;
 }
 
 export const AllTracksContainer = ({ tracks }: AllTracksContainerProps) => {
+  const [page, setPage] = useState<number>(1);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [limit, setLimit] = useState<number>(10);
   const [tracklist, setTrackList] = useState(tracks);
 
   const getNextPage = async () => {
-    fetch(`${process.env.NEXT_PUBLIC_API}/track/all`, {
-      method: "GET",
-    });
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API}/track/all?page=${page + 1}`,
+        {
+          method: "GET",
+        }
+      );
+
+      const results = await response.json();
+      setPage(results.currentPage);
+      if (results.currentPage === results.totalPages) setHasMore(false);
+      const newTracklist = [...tracklist, ...results.data];
+      setTrackList(newTracklist);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <Box>
       <InfiniteScroll
-        dataLength={10}
-        next={() => getNextPage()}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
+        dataLength={tracklist.length}
+        next={getNextPage}
+        hasMore={hasMore}
+        loader={<AllTracksLoading key={0} />}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
       >
         <Container>
           <SimpleGrid columns={[1, 1, 2, 3]} gap={6} spacing="40px">
-            {tracklist.map((track: any) => {
+            {tracklist.map((track: any, index: number) => {
               return (
                 <TrackCard
-                  key={track.title}
+                  key={`${track._id}-${index}`}
                   artist={track.name}
                   title={track.title}
                   art={track.art}
---
+++ src/components/allTracksLoading.tsx
@@ -0,0 +1,17 @@
+import { Container, CircularProgress, Box } from "@chakra-ui/react";
+
+export const AllTracksLoading = () => {
+  return (
+    <Container>
+      <Box
+        padding={20}
+        width="100%"
+        display={"flex"}
+        alignItems="center"
+        justifyContent={"center"}
+      >
+        <CircularProgress isIndeterminate color="green.300" />
+      </Box>
+    </Container>
+  );
+};
