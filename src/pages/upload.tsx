a40a5a3188ec53f19ae060ab7d9e5334b7545139
Putumani <putumaninomzaza@gmail.com>
2023-07-12 19:59:17 +0200

    modification

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Center, Heading, Button } from '@chakra-ui/react';
 import { useDropzone } from 'react-dropzone';
 import { useCallback, useState } from 'react';
 import axios from 'axios';
@@ -19,12 +19,12 @@ const UploadPage: React.FC = () => {
     formData.append('mp3', uploadedFile);
 
     try {
-      const response = await axios.post('/upload/audio', formData, {
+      const response = await axios.post('/api/upload/', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      
+
       // Handle response from the server
       console.log(response.data);
     } catch (error) {
@@ -34,31 +34,41 @@ const UploadPage: React.FC = () => {
   };
 
   return (
-    <Box p={4}>
-      <Heading as="h1" mb={4}>
-        Upload Page
-      </Heading>
-      <Box
-        {...getRootProps()}
-        p={4}
-        border="2px"
-        borderColor={isDragActive ? 'blue.500' : 'gray.200'}
-        borderRadius="md"
-        textAlign="center"
-        cursor="pointer"
-      >
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the file here...</p>
-        ) : (
-          <p>Drag and drop an .mp3 file here, or click to select file</p>
-        )}
+    <Box minHeight="100vh" display="flex" flexDirection="column">
+      <Box as="header" p={4} bg="gray.200">
+        <Heading as="h1" size="xl" textAlign="center">
+          Upload Music
+        </Heading>
       </Box>
-      <button onClick={handleUpload}>Upload</button>
+
+      <Center flex={1} py={8}>
+        <Box
+          {...getRootProps()}
+          p={4}
+          border="2px"
+          borderColor={isDragActive ? 'blue.500' : 'gray.200'}
+          borderRadius="md"
+          textAlign="center"
+          cursor="pointer"
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? <p>Drop the file here...</p> : <p>Drag and drop an .mp3 file here, or click to select file</p>}
+          {uploadedFile && (
+            <Box mt={4}>
+              <Heading as="h3" size="md">
+                Uploaded File:
+              </Heading>
+              <p>{uploadedFile.name}</p>
+            </Box>
+          )}
+        </Box>
+      </Center>
+
+      <Box as="footer" p={4} bg="gray.200" textAlign="center">
+        <Button onClick={handleUpload}>Upload</Button>
+      </Box>
     </Box>
   );
 };
 
 export default UploadPage;
-
-
