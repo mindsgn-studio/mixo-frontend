f3d9ca9dddcc559716ccd9af8d64ef1258b387c0
mindsgn <seni.tembe@protonmail.com>
2024-03-08 18:14:44 +0200

    query artist and title

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,13 +5,26 @@ const handler = async (req: any, res: any) => {
   //@ts-ignore
   const client = await clientPromise;
   const db = await client.db(`${process.env.MONGO_DB}`);
-
+  const {query} = req
+  const {search} = query
+  
   try {
+    let query = {};
+
+    if (search) {
+      query = {
+        $or: [
+          { artist: { $regex: search, $options: 'i' } }, // Case-insensitive search for artist
+          { title: { $regex: search, $options: 'i' } }, // Case-insensitive search for title
+        ],
+      };
+    }
+
     const tracks = await db
         .collection("tracks")
-        .find({})
-        .limit(10)
+        .find(query)
         .sort({ timestamp: -1 })
+        .limit(10)
         .toArray();
 
     return res.status(200).json({ tracks });
