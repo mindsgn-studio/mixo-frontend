908ce2adb6f1150a124c5d70aea04be3add70167
mindsgn <seni.tembe@protonmail.com>
2024-03-08 18:40:13 +0200

    pagination

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,33 +1,38 @@
 //@ts-ignore
-import { clientPromise } from "../../utility/database";
+import { clientPromise } from '../../utility/database';
 
 const handler = async (req: any, res: any) => {
   //@ts-ignore
   const client = await clientPromise;
   const db = await client.db(`${process.env.MONGO_DB}`);
-  const {query} = req
-  const {search} = query
-  
+  const { query } = req;
+  const { search, page = 1 } = query;
+  const limit = 10;
+  const skip = (parseInt(page as string) - 1) * limit;
+
   try {
     let query = {};
 
     if (search) {
       query = {
         $or: [
-          { artist: { $regex: search, $options: 'i' } }, // Case-insensitive search for artist
-          { title: { $regex: search, $options: 'i' } }, // Case-insensitive search for title
-        ],
+          { artist: { $regex: search, $options: 'i' } },
+          { title: { $regex: search, $options: 'i' } }
+        ]
       };
 
-      await db.collection('searches').insertOne({ query: search, timestamp: new Date() });
+      await db
+        .collection('searches')
+        .insertOne({ query: search, timestamp: new Date() });
     }
 
     const tracks = await db
-        .collection("tracks")
-        .find(query)
-        .sort({ timestamp: -1 })
-        .limit(10)
-        .toArray();
+      .collection('tracks')
+      .find(query)
+      .sort({ timestamp: -1 })
+      .skip(skip)
+      .limit(limit)
+      .toArray();
 
     return res.status(200).json({ tracks });
   } catch (error: any) {
