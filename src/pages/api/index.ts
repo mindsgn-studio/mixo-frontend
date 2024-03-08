009f09929f9767b56f03f25a99f26d765df059e8
mindsgn <seni.tembe@protonmail.com>
2024-03-08 16:56:01 +0200

    update

---
+++ src/pages/api/index.ts
@@ -0,0 +1,27 @@
+//@ts-ignore
+import { clientPromise } from "../../utility/database";
+
+const handler = async (req: any, res: any) => {
+  //@ts-ignore
+  const client = await clientPromise;
+  const db = await client.db(`${process.env.MONGO_DB}`);
+
+  
+  try {
+    const { query } = req
+    const { search } = query;
+    const tracks = await db
+        .collection("tracks")
+        .find({})
+        .sort({ timestamp: -1 })
+        .toArray();
+    if (search) {
+    }
+
+    return res.status(200).json({ tracks });
+  } catch (error: any) {
+    return res.status(303).json({});
+  }
+};
+
+export default handler;
--- src/pages/manifesto.tsx
+++
@@ -1,3 +0,0 @@
-export default function Manifesto() {
-  return <></>;
-}
