65543c10034abe9e1b8adebeb99a8cc23cb875f7
mindsgn <seni.tembe@protonmail.com>
2024-03-11 13:04:48 +0200

    update

---
+++ src/pages/api/random.ts
@@ -0,0 +1,23 @@
+//@ts-ignore
+import { clientPromise } from '../../utility/database';
+
+const handler = async (req: any, res: any) => {
+  //@ts-ignore
+  const client = await clientPromise;
+  const db = await client.db(`${process.env.MONGO_DB}`);
+  const limit = 10;
+
+  try {
+    const tracks = await db
+    .collection('tracks')
+    .aggregate([{ $sample: { size: limit } }])
+    .toArray();
+  
+    return res.status(200).json({ tracks });
+  } catch (error: any) {
+    console.log(error)
+    return res.status(303).json({});
+  }
+};
+
+export default handler;
